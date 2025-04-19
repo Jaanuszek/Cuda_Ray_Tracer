18459a1ff87adcb1dce620dcb2b2d0fb3d39e8d4
jjablosnki <kubex564@gmail.com>
2025-04-19 20:52:07 +0200

    added red circle

--- Cuda_Ray_Tracer/CudaRuntime1/main.cu
+++ Cuda_Ray_Tracer/CudaRuntime1/main.cu
@@ -29,8 +29,22 @@ __device__ unsigned char float_to_unchar(float c)
 	return static_cast<unsigned char>(255.999f * fminf(fmaxf(c, 0.0f), 1.0f));
 }
 
+__device__ bool hit_sphere(const point3& center, float radius, const ray& r)
+{
+	//distance between the ray and the center of circle
+	vec3 oc = r.get_origin() - center;
+	auto a = dot(r.get_direction(), r.get_direction());
+	auto b = 2.0f * dot(oc, r.get_direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
 __device__ color ray_color(const ray& r)
 {
+	if (hit_sphere(point3(0, 0, -1), 0.5f, r)) {
+		return color(1.0f, 0.0f, 0.0f);
+	}
 	vec3 unit_direction = unit_vector(r.get_direction());
 	auto a = 0.5f * (unit_direction.y() + 1.0f);
 	return (1.0f - a) * color(1.0f, 1.0f, 1.0f) + a * color(0.5f, 0.7f, 1.0f);
